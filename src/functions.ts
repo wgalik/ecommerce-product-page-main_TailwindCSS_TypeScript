5dd4a18238308fa1f73b6759946ead9608f06ddf
Wojtek Galik <w.galik@poczta.fm>
2025-11-29 18:18:25 +0100

    update

--- src/functions.ts
+++ src/functions.ts
@@ -10,20 +10,33 @@ export const addActiveClass = (thumbnails: Array<HTMLElement>) => {
   activeLightboxThumbnail?.classList.add(...store.activeThumbnailClassList);
   activeLightboxThumbnail?.classList.add("active");
 };
+
 export const addToCart = (badgeSpan: HTMLSpanElement) => {
   if (!store.counter) return (badgeSpan.style.display = "none");
   badgeSpan.style.display = "inline";
   badgeSpan.innerHTML = String(store.counter);
 };
-export const handleLightbox = (lightbox: HTMLElement) => {
-  checkWindowWidth();
-  if (store.windowInnerWidthREM < store.smBreakpointRem) return;
-  openLightbox(lightbox);
+
+export const closeCart = (aside: HTMLElement) => {
+  aside.classList.remove("grid");
+  store.isCartOpen = false;
 };
 
-const openLightbox = (lightbox: HTMLElement) => {
-  lightbox.classList.add("sm:flex");
-  store.isLightboxOpen = !store.isLightboxOpen;
+export const closeMenu = (
+  mainMenu: HTMLUListElement,
+  hamburgerBtn: HTMLButtonElement,
+  bgDark: HTMLDivElement,
+) => {
+  mainMenu.classList.remove("left-0");
+  bgDark.classList.add("hidden");
+  hamburgerBtn.style.backgroundImage = `url("../images/icon-menu.svg")`;
+  store.isMenuOpen = false;
+};
+
+export const closeLightbox = (lightbox: HTMLElement) => {
+  lightbox.classList.remove("sm:flex");
+
+  store.isLightboxOpen = false;
 };
 
 export const compute = (
@@ -37,12 +50,21 @@ export const compute = (
   renderCounter(counterBtn, counterSpan);
 };
 
-const checkWindowWidth = () => {
-  const windowInnerWidthPX = window.innerWidth;
-  const clientFontSize = parseFloat(
-    getComputedStyle(document.documentElement).fontSize,
-  );
-  store.windowInnerWidthREM = windowInnerWidthPX / clientFontSize;
+export const handleArrow = (
+  event: KeyboardEvent,
+  thumbnails: Array<HTMLElement>,
+) => {
+  const value = event.code === "ArrowLeft" ? "prev" : "next";
+  slideImage(value, thumbnails);
+};
+
+export const handleButton = (
+  event: MouseEvent,
+  thumbnails: Array<HTMLElement>,
+) => {
+  const target = event.currentTarget as HTMLButtonElement;
+  const value = target.value;
+  slideImage(value, thumbnails);
 };
 
 export const handleCart = (aside: HTMLElement, cartBtn: HTMLButtonElement) => {
@@ -62,16 +84,6 @@ export const handleCart = (aside: HTMLElement, cartBtn: HTMLButtonElement) => {
   openCart(aside);
 };
 
-const openCart = (aside: HTMLElement) => {
-  aside.classList.add("grid");
-  store.isCartOpen = !store.isCartOpen;
-};
-
-export const closeCart = (aside: HTMLElement) => {
-  aside.classList.remove("grid");
-  store.isCartOpen = false;
-};
-
 export const handleMenu = (
   mainMenu: HTMLUListElement,
   hamburgerBtn: HTMLButtonElement,
@@ -81,35 +93,24 @@ export const handleMenu = (
   openMenu(mainMenu, hamburgerBtn, bgDark);
 };
 
-const openMenu = (
-  mainMenu: HTMLUListElement,
-  hamburgerBtn: HTMLButtonElement,
-  bgDark: HTMLDivElement,
-) => {
-  mainMenu.classList.add("left-0");
-  bgDark.classList.remove("hidden");
-  hamburgerBtn.style.backgroundImage = `url("../images/icon-close.svg")`;
-  store.isMenuOpen = !store.isMenuOpen;
+export const handleLightbox = (lightbox: HTMLElement) => {
+  checkWindowWidth();
+  if (store.windowInnerWidthREM < store.smBreakpointRem) return;
+  openLightbox(lightbox);
 };
 
-export const closeMenu = (
-  mainMenu: HTMLUListElement,
-  hamburgerBtn: HTMLButtonElement,
-  bgDark: HTMLDivElement,
+export const handleThumbnail = (
+  event: MouseEvent,
+  thumbnails: Array<HTMLElement>,
 ) => {
-  mainMenu.classList.remove("left-0");
-  bgDark.classList.add("hidden");
-  hamburgerBtn.style.backgroundImage = `url("../images/icon-menu.svg")`;
-  store.isMenuOpen = false;
-};
-
-const removeActiveClass = () => {
-  const activeThumbnails =
-    document.querySelectorAll<HTMLDivElement>(".active")!;
-  activeThumbnails.forEach((active) => {
-    active.classList.remove(...store.activeThumbnailClassList);
-    active.classList.remove("active");
-  });
+  console.log(thumbnails);
+  const target = event.currentTarget as HTMLDivElement;
+  if (!target.attributes[0].nodeValue) return;
+  const imageNumber = Number(target.attributes[0].nodeValue.slice(14, 15));
+  store.index = imageNumber - 1;
+  removeActiveClass();
+  addActiveClass(thumbnails);
+  showImage();
 };
 
 export const renderCounter = (
@@ -130,35 +131,45 @@ export const showImage = () => {
   );
 };
 
-export const handleThumbnail = (
-  event: MouseEvent,
-  thumbnails: Array<HTMLElement>,
-) => {
-  console.log(thumbnails);
-  const target = event.currentTarget as HTMLDivElement;
-  if (!target.attributes[0].nodeValue) return;
-  const imageNumber = Number(target.attributes[0].nodeValue.slice(14, 15));
-  store.index = imageNumber - 1;
-  removeActiveClass();
-  addActiveClass(thumbnails);
-  showImage();
+const addition = () =>
+  !store.index ? (store.index = store.productImages.length - 1) : store.index--;
+
+const checkWindowWidth = () => {
+  const windowInnerWidthPX = window.innerWidth;
+  const clientFontSize = parseFloat(
+    getComputedStyle(document.documentElement).fontSize,
+  );
+  store.windowInnerWidthREM = windowInnerWidthPX / clientFontSize;
 };
 
-export const handleArrow = (
-  event: KeyboardEvent,
-  thumbnails: Array<HTMLElement>,
-) => {
-  const value = event.code === "ArrowLeft" ? "prev" : "next";
-  slideImage(value, thumbnails);
+const openCart = (aside: HTMLElement) => {
+  aside.classList.add("grid");
+  store.isCartOpen = !store.isCartOpen;
 };
 
-export const handleButton = (
-  event: MouseEvent,
-  thumbnails: Array<HTMLElement>,
+const openMenu = (
+  mainMenu: HTMLUListElement,
+  hamburgerBtn: HTMLButtonElement,
+  bgDark: HTMLDivElement,
 ) => {
-  const target = event.currentTarget as HTMLButtonElement;
-  const value = target.value;
-  slideImage(value, thumbnails);
+  mainMenu.classList.add("left-0");
+  bgDark.classList.remove("hidden");
+  hamburgerBtn.style.backgroundImage = `url("../images/icon-close.svg")`;
+  store.isMenuOpen = !store.isMenuOpen;
+};
+
+const openLightbox = (lightbox: HTMLElement) => {
+  lightbox.classList.add("sm:flex");
+  store.isLightboxOpen = !store.isLightboxOpen;
+};
+
+const removeActiveClass = () => {
+  const activeThumbnails =
+    document.querySelectorAll<HTMLDivElement>(".active")!;
+  activeThumbnails.forEach((active) => {
+    active.classList.remove(...store.activeThumbnailClassList);
+    active.classList.remove("active");
+  });
 };
 
 const slideImage = (value: string, thumbnails: Array<HTMLElement>) => {
@@ -175,15 +186,7 @@ const slideImage = (value: string, thumbnails: Array<HTMLElement>) => {
   showImage();
 };
 
-const addition = () =>
-  !store.index ? (store.index = store.productImages.length - 1) : store.index--;
 const subtraction = () =>
   store.index === store.productImages.length - 1
     ? (store.index = 0)
     : store.index++;
-
-export const closeLightbox = (lightbox: HTMLElement) => {
-  lightbox.classList.remove("sm:flex");
-
-  store.isLightboxOpen = false;
-};
