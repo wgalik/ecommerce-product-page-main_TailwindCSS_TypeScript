87ce457710f364d3a2b22c76c1767428d7b58db3
Wojtek Galik <w.galik@poczta.fm>
2025-11-29 20:35:24 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  // base: "/",
+  base: "/ecommerce-product-page-main_TailwindCSS_TypeScript/",
   plugins: [tailwindcss()],
 });
